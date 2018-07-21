edbb7ce602a2ba44e350ff5f24028199bf16a17d
Necro <Kramp_A@web.de>
2018-07-21 23:22:14 +0200

    removed probes

--- src/main/java/convenientfoundation/common/item/ModItems.java
+++ src/main/java/convenientfoundation/common/item/ModItems.java
@@ -3,7 +3,6 @@ package convenientfoundation.common.item;
 import convenientfoundation.common.item.base.CFItem;
 import convenientfoundation.common.item.base.EnumItemCategory;
 import convenientfoundation.common.item.probe.ItemBlockProbe;
-import convenientfoundation.common.item.probe.ItemHeatProbe;
 import convenientfoundation.libs.LibItems;
 import convenientfoundation.libs.LibMod;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -14,31 +13,28 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(modid = LibMod.MODID)
+@GameRegistry.ObjectHolder(LibMod.MODID)
 public class ModItems {
-
-    public static final Item WRENCH = new ItemWrench();
-    public static final Item SLIME_BUCKET = new ItemSlimeBucket();
-
-    public static final Item BLOCK_PROBE = new ItemBlockProbe();
-    public static final Item HEAT_PROBE = new ItemHeatProbe();
+    public static Item WRENCH = new ItemWrench();
+    public static Item SLIME_BUCKET = new ItemSlimeBucket();
     //
-    public static final Item ENDER_HEART = new CFItem(LibItems.ender_heart).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
-    public static final Item MOLTEN_CORE = new CFItem(LibItems.molten_core).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
-    public static final Item ARCANE_DUST = new CFItem(LibItems.arcane_dust).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
-    public static final Item DRAGON_SCALE = new CFItem(LibItems.dragon_scale).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
-    public static final Item GEODE = new ItemGeode();
+    public static Item ENDER_HEART = new CFItem(LibItems.ender_heart).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
+    public static Item MOLTEN_CORE = new CFItem(LibItems.molten_core).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
+    public static Item ARCANE_DUST = new CFItem(LibItems.arcane_dust).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
+    public static Item DRAGON_SCALE = new CFItem(LibItems.dragon_scale).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
+    public static Item GEODE = new ItemGeode();
     //
-    public static final Item IRON_ROD = new CFItem(LibItems.iron_rod).setCategory(EnumItemCategory.CRAFTING_MATERIAL).setDefaultInfo(false);
-    public static final Item GOLD_ROD = new CFItem(LibItems.gold_rod).setCategory(EnumItemCategory.CRAFTING_MATERIAL).setDefaultInfo(false);
-    public static final Item DIAMOND_SHARD = new CFItem(LibItems.diamond_shard).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
-    public static final Item EMERALD_SHARD = new CFItem(LibItems.emerald_shard).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
-
+    public static Item IRON_ROD = new CFItem(LibItems.iron_rod).setCategory(EnumItemCategory.CRAFTING_MATERIAL).setDefaultInfo(false);
+    public static Item GOLD_ROD = new CFItem(LibItems.gold_rod).setCategory(EnumItemCategory.CRAFTING_MATERIAL).setDefaultInfo(false);
+    public static Item DIAMOND_SHARD = new CFItem(LibItems.diamond_shard).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
+    public static Item EMERALD_SHARD = new CFItem(LibItems.emerald_shard).setCategory(EnumItemCategory.RESOURCE).setDefaultInfo(false);
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> evt) {
@@ -46,8 +42,6 @@ public class ModItems {
 
         r.register(WRENCH);
         r.register(SLIME_BUCKET);
-        r.register(BLOCK_PROBE);
-        r.register(HEAT_PROBE);
         //
         r.register(ENDER_HEART);
         r.register(MOLTEN_CORE);
@@ -80,8 +74,6 @@ public class ModItems {
         registerModelLocation(WRENCH);
         registerModelLocation(SLIME_BUCKET,0,SLIME_BUCKET.getRegistryName());
         registerModelLocation(SLIME_BUCKET,1,new ResourceLocation(LibMod.MODID,"magma_cube_bucket"));
-        registerModelLocation(BLOCK_PROBE);
-        registerModelLocation(HEAT_PROBE);
         //
         registerModelLocation(ENDER_HEART);
         registerModelLocation(MOLTEN_CORE);
